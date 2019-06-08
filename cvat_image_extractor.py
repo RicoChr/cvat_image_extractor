a17137932fd724426f3eb9a2bc538c85111c97cd
Rico Christiansen <webmaster@rchristiansen.de>
2019-06-08 15:40:46 +0200

    padding implemented

--- cvat_image_extractor.py
+++ cvat_image_extractor.py
@@ -7,7 +7,6 @@ import cv2
 import os
 
 
-
 def getTracksNodeForFrame(documentRoot, frameIndex):
     tracks = documentRoot.findall('track')
     if tracks is None:
@@ -23,12 +22,15 @@ def getTracksNodeForFrame(documentRoot, frameIndex):
 
     return foundTracks
 
-def extractROI(frame, xtl, ytl, xbr, ybr):
-    return frame[ytl:ybr, xtl:xbr]
 
-def exportFrameToFile(filename, img):
+def extractROI(frame, xtl, ytl, xbr, ybr, padding):
+    _xtl = max(0, xtl-padding)
+    _ytl = max(0, ytl-padding)
+    _xbr = min(frame.shape[1]-1, xbr+padding)
+    _ybr = min(frame.shape[0]-1, ybr+padding)
+    return frame[_ytl:_ybr, _xtl:_xbr]
 
-    return
+
 
 
 # ------- MAIN -------
@@ -42,11 +44,13 @@ def main():
         print(" OPTIONAL:")
         print(" --video    |  video file name. different video file name than specified in annotation file")
         print(" --padding  |  in pixels. extract additional area around roi, if present")
+        print(" --ft       |  time to display current frame. 0 -> no display")
         exit(1)
 
     annotationFile = ""
     videoFile = ""
     padding = 0
+    frameTime = 10
 
     skipIteration = False
     for i in range(1, len(sys.argv)):
@@ -66,6 +70,10 @@ def main():
 
         elif sys.argv[i] == "--padding":
             padding = int(sys.argv[i+1])
+            skipIteration = True
+
+        elif sys.argv[i] == "--ft":
+            frameTime = int(sys.argv[i+1])
             skipIteration = True
 
         else:
@@ -93,8 +101,9 @@ def main():
         ret, frame = capture.read()
         if frame is None:
             break
-        cv2.imshow("Entire Frame", frame)
-        cv2.waitKey(10)
+        if frameTime != 0:
+            cv2.imshow("Entire Frame", frame)
+
         currentTracks = getTracksNodeForFrame(root, frameCounter)
         if len(currentTracks) > 0:
             print("frame " + currentTracks[0].find('box').get('frame'), end='')
@@ -107,13 +116,18 @@ def main():
                 ytl = round(float(currentTrack.find('box').get('ytl')))
                 xbr = round(float(currentTrack.find('box').get('xbr')))
                 ybr = round(float(currentTrack.find('box').get('ybr')))
-                roi = extractROI(frame, xtl, ytl, xbr, ybr)
-                cv2.imshow("ROI", roi)
+                roi = extractROI(frame, xtl, ytl, xbr, ybr, padding)
+                display_roi = roi.copy()
+                cv2.rectangle(display_roi, (padding, padding), (display_roi.shape[1]-1-padding, display_roi.shape[0]-1-padding), (0, 255, 0))
+                if frameTime != 0:
+                    cv2.imshow("ROI", display_roi)
                 directory = "cvat_ie_output/" + currentTrack.get('label') + "/"
                 if not os.path.exists(directory):
                     os.makedirs(directory)
                 cv2.imwrite(directory + str(labelCountsDict[currentTrack.get('label')]) + ".png", roi)
                 labelCountsDict[currentTrack.get('label')] += 1
+        if frameTime != 0:
+            cv2.waitKey(frameTime)
         frameCounter+=1
 
 
